8fc05e28353b24d550a43da632519f2b17fa2383
ManshiD <manshi.desai@gmail.com>
2024-08-11 03:14:48 +0530

    Lab003 Added

---
+++ src/ex_10082024/Lab003.py
@@ -0,0 +1,15 @@
+#Python program
+print("Hello world!")
+
+# builtins.py all the important already built by Python guys
+# These are like utilities -
+
+print("Hello", "World", "Amit", "Manshi", 123, 3.14, True)
+print("Hello", "World", "Amit", "Manshi", 123, 3.14, True, sep="-")
+print("Hello", "World", "Amit", "Manshi", 123, 3.14, True, sep="|", end="_")
+print("Pramod")
+# Above example is not using a new line and so we used underscore to continue.
+print("Hello", "World", "Amit", "Manshi", 123, 3.14, True, sep="|")
+print("Pramod")
+print("Hello", "World", "Amit", "Manshi", 123, 3.14, True, sep="|", end="   ")
+print("Pramod")
